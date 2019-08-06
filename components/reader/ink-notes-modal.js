19853bf3dad4f9552cf3a30bb0a8b8212c6ffbc9
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-08-06 16:37:33 -0400

    fix: ink-notes-modal formatting

--- components/reader/ink-notes-modal.js
+++ components/reader/ink-notes-modal.js
@@ -1,4 +1,4 @@
-import { Quill } from '/js/vendor/quill.js'
+import Quill from '../../js/vendor/quill.js'
 import DOMPurify from 'dompurify'
 import { createModal, closer, opener } from '../utils/create-modal.js'
 import { modalHeader } from '../widgets/modalHeader.js'
@@ -14,38 +14,72 @@ const purifyConfig = {
 }
 
 const renderer = ({ note, saver, deleter }, config) => {
-  return html`<header class="ModalHeader">
-    ${iconButton({
-    name: 'cancel',
-    click: () => closer(),
-    autofocus: true,
-    label: 'Close Menu'
-  })}
-  <h2 class="ModalHeader-title" data-autofocus="true">Comment</h2>
-  <span></span>
-</header>
-<ink-comment class="NotesModal" .note=${note} .saver=${saver} .deleter=${deleter}></ink-comment>
-<div class="Row"><button class="TextButton TextButton--warning" @click=${() => {
-    opener('delete-highlight', { deleter, id: note.id }, 'Delete Highlight?')
-  }}>Delete Highlight</button><button class="Button" @click=${() =>
-  closer()}>Done</button></div>`
+  return html`
+    <header class="ModalHeader">
+      ${
+        iconButton({
+          name: 'cancel',
+          click: () => closer(),
+          autofocus: true,
+          label: 'Close Menu'
+        })
+      }
+      <h2 class="ModalHeader-title" data-autofocus="true">Comment</h2>
+      <span></span>
+    </header>
+    <ink-comment
+      class="NotesModal"
+      .note="${note}"
+      .saver="${saver}"
+      .deleter="${deleter}"
+    ></ink-comment>
+    <div class="Row">
+      <button
+        class="TextButton TextButton--warning"
+        @click="${
+          () => {
+            opener(
+              'delete-highlight',
+              { deleter, id: note.id },
+              'Delete Highlight?'
+            )
+          }
+        }"
+      >
+        Delete Highlight</button
+      ><button class="Button" @click="${() => closer()}">Done</button>
+    </div>
+  `
 }
 
 createModal('ink-notes', renderer, {})
 
 const renderConfirm = ({ deleter, id }) => {
   return html`
-  ${modalHeader({ title: 'Delete Highlight?' })}
-  <confirm-action dangerous slot="modal-body" .action=${() => {
-    return Promise.resolve()
-      .then(() => {
-        deleter(id)
-      })
-      .then(() => {
-        closer()
-      })
-  }} name="Delete" .view=${() =>
-  html`<p>Are you sure you want to delete this Highlight?</p>`}></confirm-action>`
+    ${modalHeader({ title: 'Delete Highlight?' })}
+    <confirm-action
+      dangerous
+      slot="modal-body"
+      .action="${
+        () => {
+          return Promise.resolve()
+            .then(() => {
+              deleter(id)
+            })
+            .then(() => {
+              closer()
+            })
+        }
+      }"
+      name="Delete"
+      .view="${
+        () =>
+          html`
+            <p>Are you sure you want to delete this Highlight?</p>
+          `
+      }"
+    ></confirm-action>
+  `
 }
 
 createModal('delete-highlight', renderConfirm)
@@ -137,42 +171,58 @@ window.customElements.define('ink-comment', InkComment)
 
 export const preview = () => {
   const cache = new Map()
-  return html`<button id="test-button" @click=${event =>
-    opener(
-      'ink-notes',
-      {
-        note: {
-          content:
-            cache.get('/note-id1') ||
-            '<blockquote data-original-quote><p>Original Quote</p></blockquote><p>Test1</p>',
-          id: '/note-id1'
-        },
-        saver (id, content) {
-          console.log(id, content)
-        },
-        deleter (id) {
-          console.log(id, 'deleted!')
-        }
-      },
-      'Label This'
-    )}>Button1</button><button  id="test-button2" @click=${event =>
-    opener(
-      'ink-notes',
-      {
-        note: {
-          content:
-            cache.get('/note-id2') ||
-            '<blockquote data-original-quote><p>Original Quote</p><ul><li>This is a list</li><li><a href="http://google.com/">This is a link</a></li></ul></blockquote><p>Test2</p>',
-          id: '/note-id2'
-        },
-        saver (id, content) {
-          cache.set(id, content)
-          console.log(id, content)
-        },
-        deleter (id, content) {
-          console.log(id, 'deleted!')
-        }
-      },
-      'Label This'
-    )}>Button2</button>`
+  return html`
+    <button
+      id="test-button"
+      @click="${
+        event =>
+          opener(
+            'ink-notes',
+            {
+              note: {
+                content:
+                  cache.get('/note-id1') ||
+                  '<blockquote data-original-quote><p>Original Quote</p></blockquote><p>Test1</p>',
+                id: '/note-id1'
+              },
+              saver (id, content) {
+                console.log(id, content)
+              },
+              deleter (id) {
+                console.log(id, 'deleted!')
+              }
+            },
+            'Label This'
+          )
+      }"
+    >
+      Button1</button
+    ><button
+      id="test-button2"
+      @click="${
+        event =>
+          opener(
+            'ink-notes',
+            {
+              note: {
+                content:
+                  cache.get('/note-id2') ||
+                  '<blockquote data-original-quote><p>Original Quote</p><ul><li>This is a list</li><li><a href="http://google.com/">This is a link</a></li></ul></blockquote><p>Test2</p>',
+                id: '/note-id2'
+              },
+              saver (id, content) {
+                cache.set(id, content)
+                console.log(id, content)
+              },
+              deleter (id, content) {
+                console.log(id, 'deleted!')
+              }
+            },
+            'Label This'
+          )
+      }"
+    >
+      Button2
+    </button>
+  `
 }
